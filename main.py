681dd4665d6eebe7d824c02d2ff840567e5d5f14
JTC-dev <104396855+JTC-dev@users.noreply.github.com>
2022-05-07 18:32:33 +1000

    Update main.py
    General tidy up and improved polling to prevent Telegram disconnections.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from asyncio import constants
+import requests
 import telebot
-import time
 import constants
 from binance.client import Client
 
@@ -8,26 +8,31 @@ from binance.client import Client
 client = Client(constants.binance_api_key, constants.binance_secret)
 
 
-tb = telebot.TeleBot(constants.token) #ENTER YOUR BOT TOKEN FROM THE BOTFATHER
+
+tb = telebot.TeleBot(constants.token, parse_mode=None) #ENTER YOUR BOT TOKEN FROM THE BOTFATHER
 
 chatid = constants.chatid #ENTER YOUR CHAT ID
 notify = 0
 
-while True:
-    raw_price = client.get_avg_price(symbol='BTCUSDC')
 
-    btc_price = raw_price['price']  
+raw_price = client.get_avg_price(symbol='BTCUSDC')
 
-    @tb.message_handler(commands=['start', 'help', 'live'])
-    def send_welcome(message):
-        tb.reply_to(message, "I'm alive!")
+btc_price = raw_price['price']  
 
+not_rounded_btc_price = int(float(btc_price))
 
-    if int(float(btc_price)) < 35000 and notify == 0:
-        tb.send_message(chat_id=constants.chatid, text=f'BTC price has fallen to {btc_price} check health factor')
-        notify = notify + 1
-    elif int(float(btc_price)) > 36000 and notify == 1:
-        tb.send_message(chat_id=constants.chatid, text=f'BTC is back in a safe range at {btc_price}')
-        notify = notify - 1
+rounded_btc_price = round(not_rounded_btc_price,2)
 
-    time.sleep(5)
+@tb.message_handler(commands=['start', 'help', 'live'])
+def send_welcome(message):
+        tb.reply_to(message, f"I'm Alive! Current BTC price: ${rounded_btc_price}USD")
+
+if rounded_btc_price < 35000 and notify == 0:
+    tb.send_message(chat_id=constants.chatid, text=f'BTC price has fallen to {btc_price} check health factor')
+    notify = notify + 1
+
+elif rounded_btc_price > 36000 and notify == 1:
+    tb.send_message(chat_id=constants.chatid, text=f'BTC is back in a safe range at {btc_price}')
+    notify = notify - 1
+
+tb.infinity_polling()
